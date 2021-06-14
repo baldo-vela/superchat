efd193926aad99a31cc1cc93182d5d12c0ce3532
baldo-vela <baldo.vela+github@gmail.com>
2021-06-13 20:17:02 -0500

    Adds Sign-in functionality

--- src/App.js
+++ src/App.js
@@ -43,6 +43,18 @@ function App() {
   );
 }
 
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google </button>
+  )
+}
 
+function ChatRoom() {
+
+}
 
 export default App;
