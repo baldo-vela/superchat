d78cc8bb22700b599aef0bd70ade7fc8b03c2087
baldo-vela <baldo.vela+github@gmail.com>
2021-06-14 00:02:27 -0500

    builds ChatMessage function

--- src/App.js
+++ src/App.js
@@ -82,4 +82,18 @@ function ChatRoom() {
 
 }
 
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+
+    </div>
+  )
+}
+
 export default App;
