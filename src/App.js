dda4ba2b75f9e690f3b8727056ec3931d467ef2e
baldo-vela <baldo.vela+github@gmail.com>
2021-06-14 00:13:40 -0500

    Builds new messages form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import 'firebase/auth'
 
 import { useAuthState }      from 'react-firebase-hooks/auth';
 import { useCollectionData }  from 'react-firebase-hooks/firestore';
+import { useState } from 'react/cjs/react.production.min';
 
 firebase.initializeApp({
   //Config Information Firebase console
@@ -68,15 +69,33 @@ function ChatRoom() {
 
   const [messages] = useCollectionData(query, {idField: 'id'});
 
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+
+    const {uid, photoURL} = auth.currentUser;
+
+    await messegesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+  }
+
   return (
     <>
       <div>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
       </div>
       
-      <div>
-      
-      </div>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={ (e) => setFormValue(e.target.value)} />
+        <button type="submit">🕊</button>
+      </form>
     </>
   )
 
